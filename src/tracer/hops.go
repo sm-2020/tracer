10988ec0f5f138c719efbdc618f103d003e69326
smdeveloper <smazumder@gmail.com>
2020-08-22 23:44:25 -0400

    initial commit.

--- src/tracer/hops.go
+++ src/tracer/hops.go
@@ -1,1 +1,33 @@
-Number of hops
+package tracer
+
+import (
+    "fmt"
+    "net"
+    "syscall"
+    "time"
+)
+
+type Hop struct {
+    Status  bool
+    Addr  syscall.Sockaddr
+    TTL   int
+    Num   int
+    Duration time.Duration
+}
+
+func (h  *Hop) Domain() string {
+    ip := h.Addr.(*syscall.SockaddrInet4).Addr
+    ipString := fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
+    host, err := net.LookupAddr(ipString)
+    if err != nil {
+        return ipString
+    } else {
+        return host[0]
+    }
+}
+
+func (h *Hop) GetIP() string {
+    ip := h.Addr.(*syscall.SockaddrInet4).Addr
+    ipString := fmt.Sprintf("%v.%v.%v.%v",ip[0],ip[1],ip[2],ip[3])
+    return ipString
+}
